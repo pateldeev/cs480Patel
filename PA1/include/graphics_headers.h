7506ab7a925e8fcfd15bfee08f33d7ebfffa7786
Deev Patel <pateldeev@nevada.unr.edu>
2018-08-29 18:53:14 -0700

    fixed #define GLM_ENABLE_EXPERIMENTAL error

--- PA1/include/graphics_headers.h
+++ PA1/include/graphics_headers.h
@@ -6,15 +6,16 @@
 #define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
 
 #if defined(__APPLE__) || defined(MACOSX)
-  #include <OpenGL/gl3.h>
-  #include <OpenGL/GLU.h>
+#include <OpenGL/gl3.h>
+#include <OpenGL/GLU.h>
 #else //linux as default
-  #include <GL/glew.h>
-  //#include <GL/glu.h>
+#include <GL/glew.h>
+//#include <GL/glu.h>
 #endif
 
 // GLM for matricies
 #define GLM_FORCE_RADIANS
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -22,12 +23,13 @@
 
 #define INVALID_UNIFORM_LOCATION 0x7fffffff
 
-struct Vertex
-{
-  glm::vec3 vertex;
-  glm::vec3 color;
+struct Vertex {
+	glm::vec3 vertex;
+	glm::vec3 color;
 
-  Vertex(glm::vec3 v, glm::vec3 c): vertex(v), color(c) {}
+	Vertex(glm::vec3 v, glm::vec3 c) :
+			vertex(v), color(c) {
+	}
 };
 
 #endif /* GRAPHICS_HEADERS_H */
