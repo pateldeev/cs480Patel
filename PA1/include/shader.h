1be43feaa86d56794a20edf8ddbe4e34ca83a775
Deev Patel <pateldeev@nevada.unr.edu>
2018-08-29 18:56:49 -0700

    fixed inclusions

--- PA1/include/shader.h
+++ PA1/include/shader.h
@@ -2,23 +2,24 @@
 #define SHADER_H
 
 #include <vector>
+#include <fstream>
+#include <sstream> 
 
 #include "graphics_headers.h"
 
-class Shader
-{
-  public:
-    Shader();
-    ~Shader();
-    bool Initialize();
-    void Enable();
-    bool AddShader(GLenum ShaderType);
-    bool Finalize();
-    GLint GetUniformLocation(const char* pUniformName);
+class Shader {
+public:
+	Shader();
+	~Shader();
+	bool Initialize();
+	void Enable();
+	bool AddShader(GLenum ShaderType, const std::string fileName = "");
+	bool Finalize();
+	GLint GetUniformLocation(const char* pUniformName);
 
-  private:
-    GLuint m_shaderProg;    
-    std::vector<GLuint> m_shaderObjList;
+private:
+	GLuint m_shaderProg;
+	std::vector<GLuint> m_shaderObjList;
 };
 
 #endif  /* SHADER_H */
